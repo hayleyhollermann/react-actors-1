bf24786775deafbcd4f1dff209df980d1365d69a
Hayley Hollermann <hayleyhollermann@gmail.com>
2019-10-22 16:22:51 -0500

    edited all components

---
+++ src/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+      return (
+          <>
+          <h2>Just actors over-React-ing</h2>
+          </>
+      );
+    }
+  }
+  
+  export default About;
+  
---
+++ src/Contact/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+      return (
+          <>
+          <h2>Contact</h2>
+          <p>1(800)-OVREACT</p>
+          <p>over_reactive_actor@dramaqueens.net</p>
+          </>
+      );
+    }
+  }
+  
+  export default Contact;
+  
